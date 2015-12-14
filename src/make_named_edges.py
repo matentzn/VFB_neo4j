a1a4e1014f0d861a414258b0a3f43a993fcf5f9f
David Osumi-Sutherland <dosumis@gmail.com>
2015-12-14 15:31:52 +0000

    Script using REST API to add named relations

--- src/make_named_edges.py
+++ src/make_named_edges.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python
+
 import requests
 import sys
 import re
+import json
 
 """A simple script to make edges named (typed) for relations from all edges of of type :Related.
 Arg1 = base_uri or neo4J server
@@ -22,17 +25,18 @@ def chunks(l, n):
         yield l[i:i+n]
 
 payload = {'statements': [{'statement': 'MATCH (n)-[r:Related]->(m) RETURN n.obo_id, r.label, m.obo_id'}]}
-rels = requests.post(url = "%s/db/data/transaction/commit" % base_url, auth = (usr, pwd) , data = json.dumps(payload))
+rels = requests.post(url = "%s/db/data/transaction/commit" % base_uri, auth = (usr, pwd) , data = json.dumps(payload))
+print rels
 rj= rels.json()
 triples = rj['results'][0]['data']
 chunked_triples = chunks(triples, 100) # Executing in chunks of 100, just 'cos.
-for c in chunks:
+for c in chunked_triples:
     statements = []
     for t in c:
         subj = t['row'][0]
         rel = re.sub(' ', '_', t['row'][1]) # In case any labels have spaces
         obj = t['row'][2]
-        statement.append({ 'statement': "MATCH (n:Class),(m:Class) WHERE n.obo_id = '%s' and m.obo_id = '%s' CREATE (n)-[r:%s]->(m)" % (subj, obj, rel)})
+        statements.append({ 'statement': "MATCH (n:Class),(m:Class) WHERE n.obo_id = '%s' and m.obo_id = '%s' CREATE (n)-[r:%s]->(m)" % (subj, obj, rel)})
     payload = {'statements': statements}
     new_rel = requests.post(url = "%s/db/data/transaction/commit" % base_uri, auth = (usr, pwd) , data = json.dumps(payload))
     print new_rel.json()
