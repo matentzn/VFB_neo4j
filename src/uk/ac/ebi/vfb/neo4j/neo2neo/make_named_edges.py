130339a1cfb9f15c84e836c344a37b8d4ede641b
David Osumi-Sutherland <dosumis@gmail.com>
2016-02-17 18:37:01 +0000

    Refactored to use neo4j_connect. Tested. Works fine.

--- src/uk/ac/ebi/vfb/neo4j/neo2neo/make_named_edges.py
+++ src/uk/ac/ebi/vfb/neo4j/neo2neo/make_named_edges.py
@@ -2,7 +2,7 @@
 
 import sys
 import re
-from ..tools import neo4j_connect
+from uk.ac.ebi.vfb.neo4j.tools import neo4j_connect
 
 """A simple script to make edges named (typed) for relations from all edges of of type :Related.
 Arg1 = base_uri or neo4J server
@@ -20,29 +20,24 @@ Created on 4 Feb 2016
 # TODO: add in check of uniqueness constraint
 # Use REST calls to /db/data/schema/
 
-base_uri = sys.argv[1]
-usr = sys.argv[2]
-pwd = sys.argv[3]
-nc = neo4j_connect(base_uri, usr, pwd)
 
-statements = ["MATCH (n)-[r:Related]->(m) RETURN n.obo_id, r.label, m.obo_id"]
-try:
-    r = nc.commit_list(statements)
-except: Exception()
-    
-rj= r.json()
-triples = rj['results'][0]['data']
-statements = []
-# Iterate over, making named edges for labels (sub space for _)
-for t in triples:
-    subj = t['row'][0]
-    rel = re.sub(' ', '_', t['row'][1]) # In case any labels have spaces
-    obj = t['row'][2]
-    statements.append("MATCH (n:Class),(m:Class) WHERE n.obo_id = '%s' and m.obo_id = '%s' MERGE (n)-[r:%s]->(m)" % (subj, obj, rel))
+nc = neo4j_connect(base_uri = sys.argv[1], usr = sys.argv[2], pwd = sys.argv[3])
 
+def make_name_edges(typ):
+    statements = ["MATCH (n)-[r:%s]->(m) RETURN n.short_form, r.label, m.short_form" % typ]
+    r = nc.commit_list(statements)        
+    triples = [x['row'] for x in r[0]['data']]
+    statements = []
+    # Iterate over, making named edges for labels (sub space for _)
+    for t in triples:
+        subj = t[0]
+        rel = re.sub(' ', '_', t[1]) # In case any labels have spaces
+        obj = t[2]
+        # Merge ensures this doesn't lead to duplicated edges if already present:
+        statements.append("MATCH (n:Class),(m:Class) " \
+                          "WHERE n.short_form = '%s' and m.short_form = '%s' " \
+                          "MERGE (n)-[r:%s { type: '%s' }]->(m)" % (subj, obj, rel, typ)) 
+    print("processing %s %s statements" % (len(statements), typ))    
+    nc.commit_list_in_chunks(statements, verbose = True, chunk_length = 10000)
 
-print("processing %s statements" % len(statements))
-
-nc.commit_list_in_chunks(statements, verbose = True, chunk_length = 1000)
-
-
+make_name_edges(typ='Related')
