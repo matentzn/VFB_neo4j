3d8491f8bf92a22cf35cd0de473ce25316fb3506
David Osumi-Sutherland <dosumis@gmail.com>
2016-08-13 14:28:10 +0900

    Script to import all individuals + their classifications, from lmb DB. Does not create any other types of individuals, or classes.

---
+++ src/uk/ac/ebi/vfb/neo4j/lmb2neoKB/import_individuals_lmb2KB.py
@@ -0,0 +1,103 @@
+'''
+Created on 13 Aug 2016
+
+@author: davidos
+'''
+# A temporary expedient until import from OWL fixed, or we move to a native neo4J implementation of KB
+
+from ..tools import neo4j_connect
+import pymysql
+import sys
+
+## Requires ssh tunnel
+c = pymysql.connect(host='localhost', user='flycircuit', db='flycircuit', 
+                    cursorclass=pymysql.cursors.DictCursor, port = 3307, 
+                    charset='utf8mb4',
+                    password = 'flycircuit')
+
+
+
+nc = neo4j_connect(base_uri = sys.argv[1], usr = sys.argv[2], pwd = sys.argv[3])
+cursor = c.cursor()
+
+# Add uniqueness constratints:
+
+cypher_constraints = ["MATCH (i:Individual) ASSERT i.short_form IS UNIQUE",
+                      "MATCH (i:Individual) ASSERT i.iri IS UNIQUE"
+                      "CREATE INDEX ON :Individual(ontology_name)",
+                      "CREATE INDEX ON :Individual(label)"]
+
+nc.commit_list(cypher_constraints)
+
+cursor.execute("SELECT s.shortFormID AS subj_sfid, " \
+               "s.label AS subj_label,  " \
+               "r.shortFormID AS rel_sfid,  " \
+               "r.label as rel_label,  " \
+               "o.shortFormID AS obj_sfid,  " \
+               "o.label AS obj_label,  " \
+               "ront.baseURI AS rBase,  " \
+               "ront.short_name as ront_name  " \
+               "FROM owl_fact f  " \
+               "JOIN owl_individual s ON (f.subject=s.id)  " \
+               "JOIN owl_individual o ON (f.object=o.id)  " \
+               "JOIN owl_objectProperty r ON (f.relation = r.id)  " \
+               "JOIN ontology ront ON (r.ontology_id=ront.id)  ")  # Just bare triples.  Not pulling types. 
+
+cypher_facts = []
+
+## Warning - hard wiring base URI here!
+vfb_ind_base_uri = 'http://www.virtualflybrain.org/owl/'
+
+
+# May need some regex escapes for ind names?
+
+for d in cursor.fetchall():
+    # some relations (e.g. depicts) have no label
+    if d['rel_label']:
+        rel_label_string = ", label: '%s'" %  d['rel_label']
+    else:
+        rel_label_string = ''
+    # First create individuals if they don't already exist.  Then create triple.    
+    cypher_facts.append('MERGE (s:Individual:VFB { short_form : "%s", label : "%s" , ontology_name : "vfb", iri: "%s%s"}) ' \
+                        'MERGE (o:Individual:VFB { short_form : "%s", label : "%s" , ontology_name : "vfb", iri: "%s%s"}) ' \
+                        'MERGE (s)-[:Related { short_form : "%s" %s, iri : "%s%s" }]->(o)'\
+                        % (d['subj_sfid'], d['subj_label'], vfb_ind_base_uri, d['subj_sfid'], 
+                           d['obj_sfid'], d['obj_label'], vfb_ind_base_uri, d['obj_sfid'], 
+                           d['rel_sfid'], rel_label_string, d['rBase'], d['rel_sfid']))
+    
+nc.commit_list_in_chunks(statements = cypher_facts, verbose = True, chunk_length = 5000) 
+
+# Add type assertions for images inds from lmb:
+
+cypher_types = []
+
+cursor.execute("SELECT oc.shortFormID AS claz, " \
+               "oi.shortFormID AS ind, " \
+               "oop.shortFormID AS rel_sfid, " \
+               "oop.label AS rel_label, " \
+               "ront.baseURI AS rBase, " \
+               "ront.short_name AS ront_name " \
+               "FROM owl_individual oi " \
+               "JOIN individual_type it ON oi.id=it.individual_id " \
+               "JOIN owl_type ot ON it.type_id=ot.id " \
+               "JOIN owl_class oc ON ot.class = oc.id " \
+               "JOIN owl_objectProperty oop ON ot.objectProperty=oop.id " \
+               "JOIN ontology ront ON (oop.ontology_id=ront.id)")
+    
+for d in cursor.fetchall():
+    if not d['rel_sfid']:
+        edge = ':INSTANCEOF'
+    else:
+        edge = ":Related { short_form : '%s', label : '%s', uri : '%s%s' }" \
+                % (d['rel_sfid'], d['rel_label'], d['rBase'], d['rel_sfid'])
+                
+    cypher_types.append("MATCH (c:Class { short_form: '%s' }), (i:Individual:VFB { short_form : '%s' }) " \
+                              "MERGE (i)-[%s]->(c)" % 
+                              (d['claz'], d['ind'], edge))
+    
+nc.commit_list_in_chunks(statements = cypher_types, verbose = True, chunk_length = 5000)
+
+
+
+
+
