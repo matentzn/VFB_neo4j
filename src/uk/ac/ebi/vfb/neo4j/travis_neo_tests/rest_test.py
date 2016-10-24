66ffbfd7fda42ed2976b7a5c951a2ddce01f0fb6
David Osumi-Sutherland <dosumis@gmail.com>
2016-10-24 11:55:27 +0100

    Update rest_test.py

--- src/uk/ac/ebi/vfb/neo4j/travis_neo_tests/rest_test.py
+++ src/uk/ac/ebi/vfb/neo4j/travis_neo_tests/rest_test.py
@@ -1,6 +1,9 @@
-from uk.ac.ebi.vfb.neo4j.tools import neo4j_connect 
+from uk.ac.ebi.vfb.neo4j.tools import neo4j_connect
+import sys
 
 nc = neo4j_connect('http://localhost:7474', 'neo4j', 'neo4j')
 
-nc.commit_list("CREATE (t:Test { fu: 'bar' })"):
+if not nc.commit_list("CREATE (t:Test { fu: 'bar' })"):
+  sys.exit('1')
+  
   
