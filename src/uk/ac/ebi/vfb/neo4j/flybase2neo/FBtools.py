9a9a891f6a592e5d86a10b57ce9515cfa3ab7422
David Osumi-Sutherland <dosumis@gmail.com>
2017-01-23 17:28:40 +0000

    Draft general FB2neo class + classes with methods for adding metadata and typing in bulk.  Might better be structured as one big class?

--- src/uk/ac/ebi/vfb/neo4j/flybase2neo/FBtools.py
+++ src/uk/ac/ebi/vfb/neo4j/flybase2neo/FBtools.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 import psycopg2
+from ..tools import Neo4jConnect, chunks
+from ..vfb_neo_tools import VFBCypherGen
+import re
 
 '''
 Created on 4 Feb 2016
+
+General classes
 
 @author: davidos
 '''
@@ -20,16 +25,209 @@ def dict_cursor(cursor):
     return l
 
 
+
 def get_fb_conn():
     return psycopg2.connect(dbname = 'flybase', host ='chado.flybase.org', user = 'flybase')
 
-def test():
-    conn = get_fb_conn()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM db")
-    dc = dict_cursor(cursor)
-    for d in dc:
-        print(d.items())
+
+class FB2Neo(object):
+    """A general class for moving content between FB and Neo.
+    Includes connections to FB and neo4J and a generic method for running queries
+    SubClass this for specific transfer jobs."""
+    
+    def __init__(self, endpoint, usr, pwd):
+        """Specify Neo4J server endpoint, username and password"""
+        self._init(endpoint, usr, pwd)
+    
+    def _init(self, endpoint, usr, pwd):
+        self.conn = get_fb_conn()
+        self.nc = Neo4jConnect(endpoint, usr, pwd)
+        self.fb_base_URI = 'http://www.flybase.org/reports/'
+        
+    def run_query(self, query):
+        """Runs a query of public Flybase, 
+        returns results as interable of dicts keyed on columns names"""
+        cursor = self.conn.cursor() # Investigate with statement
+        cursor.execute(query)
+        dc  = dict_cursor(cursor)
+        cursor.close()
+        return dc
+        
+    def commit(self):
+        self.conn.commit()
+        
+    def close(self):
+        self.close()  # Investigate implementing with statement compatibility.
+        
+
+    
+    def update_features(self, fbids):
+        pass
+    
+        
+
+class FeatureRelationship(FB2Neo):
+        
+    def get_objs(self, subject_ids, chado_rel, out_rel, o_idp):
+        query_template = "SELECT s.uniquename AS subj, o.uniquename AS obj FROM feature s " \
+                        "JOIN feature_relationship fr ON fr.subject_id=s.feature_id "  \
+                        "JOIN cvterm r ON fr.type_id=r.cvterm_id "  \
+                        "JOIN feature o ON fr.object_id=o.feature_id "  \
+                        "WHERE s.uniquename IN ('%s') " \
+                        "AND r.name = '%s' "  \
+                        "AND o.uniquename like '%s'"
+        query = query_template % ("','".join(subject_ids), chado_rel, o_idp + '%')
+        dc = self.run_query(query)
+        results = []
+        for d in dc:
+            results.append((d['subj'], out_rel, d['obj']))
+        return results
+    
+    #allele - gene  R alleleOf Type object by uniquename FBgn    
+    def allele2Gene(self, subject_ids):
+        return self.get_objs(subject_ids, chado_rel='alleleof', out_rel='alleleof', o_idp='FBgn')
+    
+    # gp - transgene R associated_with Type object by uniquename FBgn
+    def gp2Transgene(self, subject_ids):
+        return self.get_objs(subject_ids, chado_rel='associated_with', out_rel='', o_idp='FBti|FBtp')
+    
+    # gp - gene associated_with Type object by uniquename FBgn   
+    def gp2Gene(self, subject_ids):
+        return self.get_objs(subject_ids, chado_rel='associated_with', out_rel='', o_idp='FBgn')
+    
+    # transgene - allele  R associated_with Type object by uniquene FBal
+    def transgene2allele(self, subject_ids):
+        return self.get_objs(subject_ids, chado_rel='associated_with', out_rel='', o_idp='FBal')
+
+        
+class nameFeatures(FB2Neo):
+    def __init__(self,):
+        # Should also pull pubs in here???
+        self.init()
+        self.query = "SELECT f.uniquename as fbid, s.name as ascii_name, stype.name AS stype, " \
+                    "fs.is_current, s.synonym_sgml as unicode_name " \
+                    "FROM feature f " \
+                    "JOIN feature_synonym fs on (f.feature_id=fs.feature_id) " \
+                    "JOIN synonym s on (fs.synonym_id=s.synonym_id) " \
+                    "JOIN cvterm stype on (s.type_id=stype.cvterm_id) " \
+                    "WHERE f.uniquename IN ('%s')"
+
+
+    def nameSynonymLookup(self, fbids):
+        """Makes unicode name primary.  Makes everything else a synonym"""
+        #stypes: symbol nickname synonym fullname
+        dc = self.run_query(self.query % "','".join(fbids))
+        results = {}
+        old_key = ''
+        for d in dc:
+            key = d['uniquename']
+            if not (key == old_key):
+                results[d['fbid']] = {}
+                results[d['fbid']]['synonyms'] = []
+            if d['stype'] == 'symbol' and d['is_current']:
+                results[d['fbid']]['label'] = d['unicode_name']
+            else:
+                results[d['fbid']]['synonyms'].append(d['ascii_name'])
+                results[d['fbid']]['synonyms'].append(d['unicode_name'])
+            old_key = key
+        return results             
+      
+    def addSynsToNeo(self, fbids):
+        names = self.nameSynonymLookup(fbids)
+        statements = []  
+        for fbid, v in names.items():            
+            statements.append("MATCH (n:short_form : '%s') SET n.label = '%s', n.synonyms = %s"  
+                              % (fbid, v['label'], str(v['synonyms'])))
+        self.nc.commit_list_in_chunks(statements)         
+
+class FeatureType(FB2Neo):
+    
+    def __init__(self):
+        self._init()
+        
+    def grossType(self, fbids):
+        query = "SELECT f.uniquename AS fbid, typ.name as ftype; FROM feature f " \
+                "JOIN cvterm c on f.type_id=c.cvterm_id " \
+                "WHERE f.uniquename in ('%s')" % "','".join(fbids)  
+        dc = self.run_query(query)
+        results = []
+        for d in dc:
+            results.append((d['fbid'], self.map_feature_type(fbid = d['fbid'], ftype = d['ftype'])))
+        return results
+        
+    def map_feature_type(self, fbid, ftype):
+        mapping = { 'transgenic_transposon': 'SO_0000796',
+                      'insertion_site': 'SO_0001218', 
+                      'transposable_element_insertion_site': 'SO_0001218',
+                      'natural_transposon_isolate_named': 'SO_0000797',
+                      'chromosome_structure_variation' : 'SO_1000183'
+                      }
+        # see if there is some alternative query method, as classification is on FB site.
+        if ftype == 'gene':
+            if re.match('FBal', fbid):
+                return 'SO_0001023'
+            else:
+                return 'SO_0000704 '
+        elif ftype in mapping.keys():
+            return mapping[ftype]
+        else:
+            return 'SO_0000110' # Sequence feature
+    
+    def addTypes2Neo(self, fbids, detail = 'gross'):
+        """Classify FlyBase features identified by a list of fbids.
+        Optionally choose detailed classification with detail = 'fine'.  
+        (This option is currently experimental)."""
+        statements = []
+        if detail == 'gross':
+            types = self.grossType(fbids) 
+        elif detail == 'fine':
+            types = self.fineType(fbids)
+        else:
+            raise ValueError('detail arg invalid %s' % detail)
+        for t in types:
+            statements.append("MATCH (p:Class { short_form: '%s' }),(c:Class { short_form: '%s' }) " \
+            "MERGE (p)<-[:SUBCLASSOF]-(c)"  % (t[1], t[0]))
+        self.nc.commit_list_in_chunks(statements)
+                           
+        
+    def abberationType(self, abbs):
+        """abbs = a list of abberation fbids
+        Returns a list of (fbid, type) tuples where type is a SO ID"""
+        
+        results = []
+        abbs_proc = [] # For tracking processed abbs
+        query = "SELECT f.uniquename AS fbid db.name AS db, dbx.accession AS acc " \
+        "FROM feature f " \
+        "JOIN cvterm gross_type ON gross_type.cvterm_id=f.type_id"
+        "JOIN feature_cvterm fc ON fc.feature_id = f.feature_id " \
+        "JOIN cvterm fine_type ON fine_type.cvterm_id = fc.cvterm_id " \
+        "JOIN feature_cvtermprop fctp ON fctp.feature_cvterm_id = fc.feature_cvterm_id " \
+        "JOIN cvterm meta ON meta.cvterm_id = fctp.type_id " \
+        "JOIN cvterm gtyp ON gtyp.cvterm_id = f.type_id " \
+        "JOIN dbxref dbx ON fine_type.dbxref_id = dbx.dbxref_id"
+        "JOIN db ON db.dbxref_id = dbx.dbxref_id"
+        "WHERE gross_type.name = 'chromosome_structure_variation' -- double checks input gross type" \
+        "AND  meta.name = 'wt_class' " \
+        "AND f.uniquename in ('%s')"
+        dc = self.run_query(query)
+        for d in dc:
+            results.append((d['fbid'], d['db'] + '_' + d['acc']))
+            abbs_proc.append(d['fbid'])
+        [results.append((a, 'SO_0000110')) for a in abbs if a not in abbs_proc] # Defaulting to generic feature id not abb
+        return results
+            
+        def fineType():
+            gt = self.grossType()
+            abbs_list = []
+            results = []
+            for g in gt:
+                if g[1] == '':
+                    abbs_list.append(g[0])
+                else:
+                    results.append(g)
+            results.extend(self.abberationType(abbs_list))
+            
+
 
     
 
---
+++ src/uk/ac/ebi/vfb/neo4j/flybase2neo/add_features.py
@@ -0,0 +1,152 @@
+'''
+Created on Oct 28, 2016
+
+@author: davidos
+'''
+
+from .FBtools import FB2Neo
+from ..tools import chunks
+import re
+from uk.ac.ebi.vfb.neo4j.flybase2neo.FBtools import FeatureRelationship,\
+    nameFeatures
+
+def generate_monarch_genotype_short_form(chado_id):
+    base_iri = 'https://monarchinitiative.org/'
+    short_form = "MONARCH_FBgeno" + chado_id
+    return { 'base_iri' : base_iri , 'short_form' : short_form, 'iri' : base_iri + short_form }
+
+def clean_sgml_tags(sgml_string):
+    sgml_string = re.sub('\<up\>', '[', sgml_string)
+    sgml_string = re.sub('\<\\up\>', ']', sgml_string)
+    sgml_string = re.sub('\<\\up\>', '[[', sgml_string)
+    sgml_string = re.sub('\<\\down>', ']]', sgml_string)
+    return sgml_string
+
+
+    
+
+class AddFeatures(FB2Neo):
+    """A general class for safely adding/updating features from FB.
+    To construct, specify neo endpoint, usr, pwd"""
+    
+    def add_genotypes_components(self, limit = False):
+        """
+        """
+        genotype_base = 'https://monarchinitiative.org/'
+        genotype_pre = 'MONARCH_FBgeno'
+        query = "SELECT DISTINCT g.id AS chado_id, g.uniquename AS genotype_name," \
+                "gcomp.uniquename AS gcomp_id, gcomp.name AS gcomp_name" \
+                "FROM genotype g " \
+                "JOIN phenstatement ps ON (ps.genotype_id = g.genotype_id) -- limit to simple phens" \
+                "JOIN feature_genotype fg ON (g.genotype_id = fg.genotype_id) " \
+                "JOIN feature gcomp ON (fg.feature_id = gcomp.feature_id) "               
+        gc = self.run_query(query)
+        statements = []
+        if limit:
+            limit_clause = 'limit 5' % limit
+        else:
+            limit_clause = ''
+        for g in gc:
+            statements.append("MATCH (gc:Class { short_form : 'GENO_0000536' }) " \
+                      "MERGE (f:Class { iri : '%s' } " \
+                      "MERGE (g:Class:genotype { iri : '%s'}) " \
+                      "MERGE (g)-[:SUBCLASSOF]->(gc) " \
+                      "MERGE (g)-[:has_part]->(f) " \
+                      "SET g.short_form = '%s' " \
+                      "SET g.name = '%s' %s"
+                        % (self.fb_base_URI + g['gcomp_id'],
+                           genotype_base + genotype_pre + g['chado_id'],
+                           genotype_pre + g['chado_id'],
+                           clean_sgml_tags(sgml_string = g['genotype_name']),
+                           limit_clause))
+                                             
+        self.nc.commit_list_in_chunks(statements)
+    
+    def gen_cypher_add_feature(self, fbid):
+        """RETURNS a cypher statement for merging a feature on its iri, adding"""
+        
+        return "MERGE (f:Class { iri : '%s' }) " \
+                "SET f.short_form = '%s, label = '%s' " % (                    
+                self.fb_base_URI + fbid, fbid, ) 
+
+   
+
+    def update_expressed_features(self):
+        query = "SELECT DISTINCT f.uniquename AS fbid, f.name" \
+               "FROM feature_expression fe " \
+               "JOIN feature f ON (fe.feature_id=f.feature_id)"
+        dc = self.run_query(query)    
+        gps = [d['fbid'] for d in dc]
+        fr = FeatureRelationship(self.endpoint, self.usr, self.pwd)
+        expressed_features = fr.gp2Transgene(gps) # Get expressed transgenes
+        expressed_features.extend(fr.gp2Gene(gps))
+        statements = []
+        for f in features:
+            statements.append(self.gen_cypher_add_feature(f))
+        self.nc.commit_list_in_chunks(statements)                    
+        return expressed_features
+            
+    
+    def upadate_expression_patterns(self):
+        features = self.update_expressed_features()
+        
+        # Problem1:  These need to be update-able, 
+        # but how can we track IDs if we have to generate new ones at each update?
+        # Can do match queries first to check whether they exist.
+        
+        
+        cs = chunks(features, 1000)
+        for c in cs:
+            statement = "MATCH (epg:Class { iri : '%s' })<-[:SUBCLASSOF]-" \
+            "(ep:Class { iri : '%s' })-[:expresses { type : 'rel' }]-" \
+            "(f:Class) WHERE f.short_form IN ('%s') " \
+            "RETURN f " % "','".join(c)
+            f = self.nc.commit_list(statement)
+        
+        
+        # Can match on names - but this will be screwed up when name updates... 
+        # Unless that update is done first... 
+        statements = []
+        for f in features:
+            statements.append("MATCH (epg:Class { iri : '%s' }),  " \
+                              "(f:Class { iri : '%s' }) " \
+                              "MERGE (epg)<-[:SUBCLASSOF]-(ep:Class { iri : '%s' } ) "  \
+                              "-[:expresses { type : 'rel' }]-(f) " \
+                              "SET ep.short_form ='%s', ep.label = '%s'"
+                              % ()
+        
+        features = self.get_expressed_features()
+        statements = []
+        for f in features:
+             statments.append(self.gen_cypher_add_feature(fbid = d['fbid'], ascii_sym = d['ascii_sym']))
+             
+        self.nc.commit_list_in_chunks()
+        nf = nameFeatures()
+        nf.addSynsToNeo(features)
+
+        
+        
+        self.nc.commit_list_in_chunks(statements)
+            
+        statements = []
+        for ef in features:
+            
+            ep_id = gen_ep_id()
+            ep_name = 
+            "CREATE (gp:Class { iri : '%s' }) " \
+            "SET f.short_form = '%s, label = '%s' 
+            statement = self.gen_cypher_add_feature(fbid = d['fbid'],
+                                                          ascii_sym = d['ascii_sym'])
+            statement += self.gen_cypher_add_feature(fbid = d['fbid'],
+                                                          ascii_sym = d['ascii_sym'])" \
+            
+                      +=             "MERGE (ep)-[:expresses]->(f) "
+                         % gen_ep_id()
+                                                          
+            
+      
+            
+        
+        
+        
+        pass
