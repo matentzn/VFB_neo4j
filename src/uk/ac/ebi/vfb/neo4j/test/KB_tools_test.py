32e48d7a7c812742ea7402adafd26e7e394a199c
David Osumi-Sutherland <dosumis@gmail.com>
2017-03-27 21:44:26 +0100

    Test for named type addition and for pulling edge label from property node

--- src/uk/ac/ebi/vfb/neo4j/test/KB_tools_test.py
+++ src/uk/ac/ebi/vfb/neo4j/test/KB_tools_test.py
@@ -14,7 +14,7 @@ class TestEdgeWriter(unittest.TestCase):
         self.edge_writer = kb_owl_edge_writer('http://localhost:7474', 'neo4j', 'neo4j')
         s = []
         s.append("MERGE (i1:Individual { IRI : 'Aya' }) " \
-            "MERGE (r1:Property { IRI : 'loves' }) " \
+            "MERGE (r1:Property { IRI : 'http://fu.bar/loves', label : 'loves' }) " \
             "MERGE (i2:Individual { IRI: 'Freddy' }) ")
         s.append("MERGE (i1:Individual { IRI : 'Aya' }) " \
             "MERGE (r1:Property { IRI : 'daughter_of' }) " \
@@ -28,27 +28,29 @@ class TestEdgeWriter(unittest.TestCase):
     def tearDown(self):
         # TODO - add some deletions here
         s = ["MATCH (i1:Individual { IRI : 'Aya' })-" \
-        "[r1:Property { IRI : 'loves' }]->" \
-        "(i2:Individual { IRI: 'Freddy' }) DELETE i1, r1, i2"]
-        s.append("MATCH (i1:Individual { IRI : 'Aya' })-" \
-        "[r1:Property { IRI : 'loves' }]->" \
-        "(i2:Individual { IRI: 'Freddy' }) DELETE i1, r1, i2")       
+       "[r1:Related { IRI : 'http://fu.bar/loves' }]->" \
+       "(i2:Individual { IRI: 'Freddy' }) DELETE i1, r1, i2"]
+        s.append("(r1:Property { IRI : 'http://fu.bar/loves'}) DELETE r1")       
         self.edge_writer.nc.commit_list(s)
         pass
 
 
     def test_add_fact(self):
 
-        self.edge_writer.add_fact(s = 'Aya', r = 'loves', o = 'Freddy', edge_annotations = { 'fu' : "ba'r", 
-                                                                                            'bin': ['bash', "ba'sh"],
-                                                                                            'i' : 1,                                                                                          
-                                                                                            'x' : True })  # TBA - put something in edge annotation
+        self.edge_writer.add_fact(s = 'Aya', r = 'http://fu.bar/loves', 
+                                  o = 'Freddy', 
+                                  edge_annotations = { 'fu' : "ba'r", 
+                                                      'bin': ['bash', "ba'sh"],
+                                                      'i' : 1,                                                                                          
+                                                      'x' : True })
         assert self.edge_writer.check_proprties() == True
         self.edge_writer.commit() 
         assert self.edge_writer.test_edge_addition() == True  
         self.edge_writer.add_fact(s = 'Aya', r = 'loved', o = 'Freddy', edge_annotations = {} )
         self.edge_writer.commit()
         assert self.edge_writer.test_edge_addition() == False
+
+        
         # Add test of added content?
         
         
@@ -58,7 +60,11 @@ class TestEdgeWriter(unittest.TestCase):
     def test_add_named_type_ax(self):
         self.edge_writer.add_named_type_ax(s = 'Aya', o = 'Person')
         self.edge_writer.commit()
-        assert self.edge_writer.test_edge_addition() == True
+        assert self.edge_writer.test_edge_addition() == True        
+        r1 = self.edge_writer.nc.commit_list(["MATCH (i1:Individual { IRI : 'Aya' })-" \
+        "[r1:Related { IRI : 'http://fu.bar/loves' }]->" \
+        "(i2:Individual { IRI: 'Freddy' }) RETURN r1.label"])
+        assert r1[0]['data'][0]['row'][0] == 'loves'
         
 class TestNodeImporter(unittest.TestCase):
 
