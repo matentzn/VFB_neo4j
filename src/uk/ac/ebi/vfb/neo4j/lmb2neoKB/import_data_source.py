eb2be30a3a9db46d2f3c45455e793dda1261ef19
David Osumi-Sutherland <dosumis@gmail.com>
2017-04-04 16:37:33 +0100

    works.  No longer adds links inds to data_sources. This happens in ind addition script.

--- src/uk/ac/ebi/vfb/neo4j/lmb2neoKB/import_data_source.py
+++ src/uk/ac/ebi/vfb/neo4j/lmb2neoKB/import_data_source.py
@@ -11,14 +11,14 @@ Requires ssh tunnel to LMB server
 
 import pymysql
 import sys
-from ..tools import neo4j_connect
+from uk.ac.ebi.vfb.neo4j.neo4j_tools import neo4j_connect
+from uk.ac.ebi.vfb.neo4j.lmb2neoKB.lmb_query_tools import get_conn
+
 
 
 nc = neo4j_connect(base_uri=sys.argv[1], usr=sys.argv[2], pwd=sys.argv[3])
-c = pymysql.connect(host='localhost', user='flycircuit', db='flycircuit', 
-                    cursorclass=pymysql.cursors.DictCursor, port = 3307, 
-                    charset='utf8mb4',
-                    password = 'flycircuit')
+c = get_conn(sys.argv[4], sys.argv[4])
+
 
 cursor = c.cursor()
 cursor.execute("SELECT * FROM data_source")
@@ -26,10 +26,11 @@ statements = []
 for d in cursor.fetchall():
     statement = "MERGE (d:data_source { name: '%s' }) " % d['name']
     if d['pub_pmid']:
-        statement += "MERGE (d)-[:has_reference]->(:pub { PMID: '%s' } )" % d['pub_pmid']
+        statement += "MERGE (p:pub { PMID: '%s' } ) " \
+        "MERGE (d)-[:has_reference]->(p)" % d['pub_pmid']
     statements.append(statement)
-    statement = ''
-    statement = "MATCH (d:data_source { name: '%s' }) " % d['name']
+    if d['dataset_link'] or d['data_link_pre'] or d['data_link_post'] or d['dataset_link']:
+        statement = "MATCH (d:data_source { name: '%s' }) " % d['name']
     if d['dataset_link']:
         statement += "SET d.dataset_link = '%s' " % d['dataset_link']
     if d['data_link_pre']:
@@ -38,21 +39,27 @@ for d in cursor.fetchall():
         statement += "SET d.data_link_post = '%s' " % d['data_link_post']
     if d['dataset_link']:
         statement += "SET d.dataset_link =  '%s' " % d['dataset_link']
+    statements.append(statement)
 nc.commit_list(statements)
+
+statements.append("CREATE CONSTRAINT ON (ds:data_set) ASSERT ds.name IS UNIQUE")
+statements.append("CREATE CONSTRAINT ON (p:pub) ASSERT p.PMID IS UNIQUE")
+nc.commit_list(statements)
+
 
         
 cursor.execute("SELECT oi.shortFormID, ds.name FROM owl_individual oi " \
                "JOIN data_source ds ON oi.source_id = ds.id " \
-               "WHERE oi.shortFormID like 'VFBi\_%'")
+               "WHERE oi.shortFormID like 'VFBc\_%'")
 
-statements = []
-
-for d in cursor.fetchall():
-    statements.append("MATCH (c:Individual { short_form : '%s' }) " \
-                      "MERGE (c)-[:has_source]->(ds:data_source { name : '%s'})" 
-                      % (d['shortFormID'], d['name']))
-
-nc.commit_list_in_chunks(statements, chunk_length = 1000)
+# statements = []
+# 
+# for d in cursor.fetchall():
+#     statements.append("MATCH (c:Individual { short_form : '%s' }), (ds:data_source { name : '%s'})" \
+#                       "MERGE (c)-[:has_source]->(ds)" 
+#                       % (d['shortFormID'], d['name']))
+# 
+# nc.commit_list_in_chunks(statements, chunk_length = 1000)
 cursor.close()
 c.close()
 
