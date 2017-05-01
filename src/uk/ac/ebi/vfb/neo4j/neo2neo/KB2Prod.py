bc98565b76a508c6343590299153104210b35963
David Osumi-Sutherland <dosumis@gmail.com>
2017-05-01 06:43:44 +0100

    fixed bugs & tested channel mover

--- src/uk/ac/ebi/vfb/neo4j/neo2neo/KB2Prod.py
+++ src/uk/ac/ebi/vfb/neo4j/neo2neo/KB2Prod.py
@@ -14,27 +14,30 @@ and all non-owl edges (not :Related, :INSTANCEOF, :SUBCLASSOF)
 and move them from KB to prod.
 """
 
-kb = neo4j_connect(sys.argv(1), sys.argv(2), sys.argv(3))
-prod = neo4j_connect(sys.argv(4), sys.argv(5), sys.argv(6))
+kb = neo4j_connect(sys.argv[1], sys.argv[2], sys.argv[3])
+prod = neo4j_connect(sys.argv[4], sys.argv[5], sys.argv[6])
 
 ncm = neo4jContentMover(kb,prod)
     
 
 ## move non-OWL content
+## Requires: IRIs for everything to be moved (as iri)
 non_owl_node_match = "MATCH (n) " \
         "WHERE not('Class' IN labels(n)) " \
         "AND not('Individual' IN labels(n)) " \
-        "AND not('Property' IN labels(n))  "        
-ncm.move_nodes(match = non_owl_node_match, key = 'iri')
-
+        "AND not('Property' IN labels(n))"     
+           
+ncm.move_nodes(match = non_owl_node_match, key = 'iri', test_mode=False)
+ 
 non_owl_edge_match = "MATCH (s)-[r]->(o) " \
              "WHERE not(type(r) IN ['INSTANCEOF', 'Related', :SUBCLASSOF]) " 
-ncm.move_edges(match = non_owl_edge_match, node_key = 'iri')
+ncm.move_edges(match = non_owl_edge_match, node_key = 'iri', test_mode=False)
 
 ## move channels and directly associated edges
+### TESTED  ## Requires all IRI -> iri in KB
 channel_match = "MATCH (:Class { label: 'channel'})-[:INSTANCEOF]-(n:Individual) "
-ncm.move_nodes(match = channel_match, key = 'iri')
+ncm.move_nodes(match = channel_match, key = 'iri', test_mode=False)
 
 edge_match = "MATCH (:Class { label: 'channel'})-[:INSTANCEOF]-(s:Individual) " \
-             "WITH n MATCH (s)-[r]-(o) "
-ncm.move_edges( match = edge_match, node_key = 'iri')
+             "WITH s MATCH (s)-[r]-(o) "
+ncm.move_edges( match = edge_match, node_key = 'iri', test_mode=False)
