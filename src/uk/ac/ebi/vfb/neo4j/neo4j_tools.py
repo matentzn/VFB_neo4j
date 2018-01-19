f7f558e978c6d955d37ba7b8ad9e56e3d84ccd1b
dosumis <dosumis@gmail.com>
2018-01-19 13:05:25 +0000

    Fixed commit_list_in_chunk call on label mover

--- src/uk/ac/ebi/vfb/neo4j/neo4j_tools.py
+++ src/uk/ac/ebi/vfb/neo4j/neo4j_tools.py
@@ -276,9 +276,9 @@ class neo4jContentMover:
             statements.append(match +
                               "SET n%s  " % lab_string)
 
-        self.To.commit_list_in_chunks(statements, chunk_length, verbose)
-
-
+        self.To.commit_list_in_chunks(statements=s,
+                                      verbose=verbose,
+                                      chunk_length=chunk_length)
                       
                        
     
