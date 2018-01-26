eaede72a6d2a835c3e1ba26e5be76be73f1208d2
dosumis <dosumis@gmail.com>
2018-01-26 17:46:39 +0000

    First draft working anat 2 pub loader in python 3.

--- src/uk/ac/ebi/vfb/neo4j/flybase2neo/add_refs_for_anat.py
+++ src/uk/ac/ebi/vfb/neo4j/flybase2neo/add_refs_for_anat.py
@@ -34,21 +34,6 @@ supported_xrefs = {'FlyBase': 'FlyBase:FBrf\d{7}',
 
 
 
-def get_dbxrefs(oboXrefs):
-    out = []
-    for xref in oboXrefs:
-        out[xref['id']] = out[xref['database']]
-    return out
-
-
-def get_def_dbxrefs():
-
-    return
-
-def get_syn_dbxrefs():
-    return
-
-
 # obo_synonym:{"name":"IDFP",
 #              "scope":"hasBroadSynonym",
 #               "type":null,"xrefs":[
@@ -57,28 +42,28 @@ def get_syn_dbxrefs():
 # ,{"name":"vmpr","scope":"hasRelatedSynonym","type":null,"xrefs":[{"database":"FlyBase","id":"FBrf0193607","description":null,"url":null}]},{"name":"LAL","scope":"hasExactSynonym","type":"BrainName official abbreviation","xrefs":[{"database":"FlyBase","id":"FBrf0224194","description":null,"url":null}]}
 
 
-def proc_xrefs(dbxrefs):
-    if not dbxrefs:
-        return False
-    out = {}
-    for db in supported_xrefs.keys():
-        out[db] = []
-    for xref in dbxrefs:
-        for db, re_string in supported_xrefs.items():
-            m = re.compile(re_string)
-            if re.match(m, xref):
-                ref = xref.split(':')[1]
-                out[db].append(ref)
-    return out
+# def proc_xrefs(dbxrefs):
+#     if not dbxrefs:
+#         return False
+#     out = {}
+#     for db in supported_xrefs.keys():
+#         out[db] = []
+#     for xref in dbxrefs:
+#         for db, re_string in supported_xrefs.items():
+#             m = re.compile(re_string)
+#             if re.match(m, xref):
+#                 ref = xref.split(':')[1]
+#                 out[db].append(ref)
+#     return out
 
 
 def roll_cypher_add_def_pub_link(sfid, pub_id_typ, pub_id):
     """Generates a Cypher statement that links an existing class
     to a pub node with the specified attribute.  Generates a new pub node
      if none exists."""
-    return  "MATCH (a:Class { short_form : '%s' }) " \
-            "MERGE (p:pub:Individual { %s : '%s' }) " \
-            "MERGE (a)-[:has_reference { typ : 'def' }]->(p)"  % (sfid, pub_id_typ, pub_id)
+    return "MATCH (a:Class { short_form : '%s' }) " \
+           "MERGE (p:pub:Individual { %s : '%s' }) " \
+           "MERGE (a)-[:has_reference { typ : 'def' }]->(p)" % (sfid, pub_id_typ, pub_id)
 
 
 def roll_cypher_add_syn_pub_link(sfid, s, pub_id_typ, pub_id):
@@ -92,17 +77,31 @@ def roll_cypher_add_syn_pub_link(sfid, s, pub_id_typ, pub_id):
 
 
 nc.commit_list(["MERGE (:pub:Individual { FlyBase: 'Unattributed' })"])
-q = nc.commit_list(["MATCH (c:Class) return short_form, c.obo_synonym as syns, c.obo_definition_citation as def"])
+q = nc.commit_list(["MATCH (c:Class|Individual) return short_form, c.obo_synonym as syns, c.obo_definition_citation as def"])
 dc = results_2_dict_list(q)
-s = []
+statements = []
 for d in dc:
     if d['def']:
         def_cit = json.loads(d['def'])
-        s.append[roll_cypher_add_def_pub_link(
+        statements.append(roll_cypher_add_def_pub_link(
             sfid = d['short_form'],
-            pub_id = d[],
-            typ = 'def',
-            )]
+            pub_id = d['id'],
+            pub_id_typ = 'def',
+            ))
+    elif d['syns']:
+        for syn in d['syns']:
+            s = json.loads(syn)
+            statements.append(
+                roll_cypher_add_syn_pub_link(
+                    sfid = d['short_form'],
+                    pub_id = d['id'],
+                    pub_id_typ = ['database'],
+                    s=s))
+
+nc.commit_list_in_chunks(statements, verbose=True, chunk_length=2000)
+
+
+
 
 
 
@@ -115,4 +114,3 @@ obo_synonym_string = '[{"name":"IDFP","scope":"hasBroadSynonym","type":null,"xre
 obo_synonym = json.loads(obo_synonym)
 obo_definition_citation_string = '{"definition":"A sense organ embedded in the integument and consisting of one or a cluster of sensory neurons and associated sensory structures, support cells and glial cells forming a single organized unit with a largely bona fide boundary.","oboXrefs":[{"database":"FlyBase","id":"FBrf0111704","description":null,"url":null}]}'
 obo_definition_citation = json.load(obo_definition_citation_string)
-nc.commit_list_in_chunks(statements, verbose= True, chunk_length = chunk_length)
