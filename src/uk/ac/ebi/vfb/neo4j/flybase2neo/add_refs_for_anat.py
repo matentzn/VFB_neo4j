0596dd2a0e658e0d2ff3451467818bb374ec9fa8
Rob Court <robbie.court@gmail.com>
2018-01-31 14:00:07 +0000

    fix for escaped single quotes
    see https://travis-ci.org/VirtualFlyBrain/pipeline/jobs/335373333#L1969

--- src/uk/ac/ebi/vfb/neo4j/flybase2neo/add_refs_for_anat.py
+++ src/uk/ac/ebi/vfb/neo4j/flybase2neo/add_refs_for_anat.py
@@ -61,9 +61,9 @@ def roll_cypher_add_def_pub_link(sfid, pub_id_typ, pub_id):
     """Generates a Cypher statement that links an existing class
     to a pub node with the specified attribute.  Generates a new pub node
      if none exists."""
-    return "MATCH (a:Class { short_form : '%s' }) " \
-           "MERGE (p:pub:Individual { %s : '%s' }) " \
-           "MERGE (a)-[:has_reference { typ : 'def' }]->(p)" % (sfid, pub_id_typ, pub_id)
+    return "MATCH (a:Class { short_form : \"%s\" }) " \
+           "MERGE (p:pub:Individual { %s : \"%s\" }) " \
+           "MERGE (a)-[:has_reference { typ : \"def\" }]->(p)" % (sfid, pub_id_typ, pub_id)
 
 
 def roll_cypher_add_syn_pub_link(sfid, s, pub_id_typ, pub_id):
