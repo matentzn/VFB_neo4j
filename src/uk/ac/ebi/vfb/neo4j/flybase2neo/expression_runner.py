792bdbcb65cfaa761044dc5da1a705c1181be4b0
dosumis <dosumis@gmail.com>
2018-02-21 12:21:06 +0000

    Added FB query for FBexp, feature & pub.

--- src/uk/ac/ebi/vfb/neo4j/flybase2neo/expression_runner.py
+++ src/uk/ac/ebi/vfb/neo4j/flybase2neo/expression_runner.py
@@ -13,35 +13,36 @@ import warnings
 endpoint = sys.argv[1]
 usr = sys.argv[2]
 pwd = sys.argv[3]
+temp_csv_filepath = sys.argv[4]
 
-fm = FeatureMover(endpoint, usr, pwd)
+fm = FeatureMover(endpoint, usr, pwd, temp_csv_filepath)
 def exp_gen(): return # code for generating and wiring up expression patterns
 
-
+def add_pubs(): return
 # Query feature_expression => pub feature and fbex
-feps = fm.query_fb("SELECT ...")
+feps = fm.query_fb("SELECT pub.uniquename as fbrf, "
+                   "f.uniquename as fbid, e.uniquename as fbex "
+                   "FROM feature_expression fe "
+                   "JOIN pub ON fe.pub_id = pub.pub_id"
+                   "JOIN feature f ON fe.feature_id = f.feature_id"
+                   "JOIN expression e ON fe.expression_id = e.expression_id")
 
-# How to structure for batch
+gene_products = [f['fbid'] for f in feps]
+pubs = [f['fbrf'] for f in feps]
+taps = [f['fbex'] for f in feps]
 
-for fep in feps:
-    # Split feps into batches for reasonable in clauses.
-    # List comp => set X to add & fill
-    pub = fep['pub'] 
-    # -> merge pub + set details (if new)
-    feat = fep['feature'] 
-    names = 
-    typ = type(feat)
-    if typ == '':
-        x = 1 # Create
-    elif typ == '':
-        x = 2
-    else:
-        warnings.warn()
-        
-    # merge feature + set details (if new)
-    # Generate expression pattern/localization pattern
-    exp_gen(fep)
-    
+# TODO - check paths through feature_relations table
+fm.add_features(gene_products)
+fm.addTypes2Neo(gene_products)
+genes = fm.gp2Gene(gene_products)
+transgenes = fm.gp2Transgene(gene_products)
+fm.add_feature_relations(genes)
+fm.add_feature_relations(transgenes)
+
+add_pubs(pubs)
+
+
+
 
 
     
